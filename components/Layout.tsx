a7e371ab40aa6955e6cbf478afc95dd922f5d559
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2022-08-19 23:41:05 +0530

    fix: add social icons

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,23 +1,43 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-
 interface Layout {
-    children: any
+  children: any;
 }
 
-
 const Layout = ({ children }: Layout) => {
-    const router = useRouter();
+  const router = useRouter();
 
   return (
     <div className="flex flex-col p-8">
-      <nav className="w-full max-w-3xl flex mx-auto">
-        <Link href="/" >
-          <a className={router.pathname === "/" ? "font-bold   md:inline-block p-1 sm:px-3 sm:py-2  transition-all" :"font-normal   md:inline-block p-1 sm:px-3 sm:py-2  transition-all" }>
-           Home
+      <nav className="w-full max-w-3xl flex mx-auto justify-between">
+        <Link href="/">
+          <a
+            className={
+              router.pathname === "/"
+                ? "font-bold   md:inline-block p-1 sm:px-3 sm:py-2  transition-all"
+                : "font-normal   md:inline-block p-1 sm:px-3 sm:py-2  transition-all"
+            }
+          >
+            Home
           </a>
         </Link>
+
+        <div>
+          <a
+            className="font-bold   md:inline-block p-1 sm:px-3 sm:py-2  transition-all"
+            href="https://twitter.com/karthik_coder"
+          >
+            Twitter
+          </a>
+
+          <a
+            className="font-bold   md:inline-block p-1 sm:px-3 sm:py-2  transition-all"
+            href="https://github.com/skarthikeyan96/skarthikeyan96"
+          >
+            Github
+          </a>
+        </div>
       </nav>
 
       <main className="flex flex-col  px-4 mt-16">
